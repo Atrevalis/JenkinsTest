3670c0f5e0a3340014689a47f415fc0107a020fc
Atrevalis <32127221+Atrevalis@users.noreply.github.com>
2021-10-03 23:59:14 +0300

    Add files via upload

---
+++ JenkinsTest/JenkinsTest/UnitTest1.cs
@@ -0,0 +1,26 @@
+using NUnit.Framework;
+
+namespace JenkinsTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            
+            Assert.Pass();
+        }
+        
+        [Test]
+        public void Test2()
+        {
+            
+            Assert.Pass();
+        }
+    }
+}
